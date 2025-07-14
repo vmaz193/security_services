1b36c9e21db08ac26714f3678d36e5ca378ded7d
vmaz193 <an.vm193@gmail.com>
2025-07-14 12:47:46 -0400

    adding PowerBI dashboard draft

--- security_transforms/models/marts/fct_finances_by_district.sql
+++ security_transforms/models/marts/fct_finances_by_district.sql
@@ -9,10 +9,10 @@ select
     r.district_id,
     i.business_unit_id,
     -- Measures
-    sum(i.monthly_revenue_usd) as total_revenue,
+    round(sum(i.monthly_revenue_usd)::numeric,2) as total_revenue,
     -- Business Logic: Allocate the corporate overhead cost evenly to each district
-    sum(i.total_corporate_costs / i.total_districts) as allocated_corporate_overhead,
-    (sum(i.monthly_revenue_usd) - sum(i.total_corporate_costs / i.total_districts)) as true_profit
+    round(sum(i.total_corporate_costs / i.total_districts)::numeric,2) as allocated_corporate_overhead,
+    round((sum(i.monthly_revenue_usd) - sum(i.total_corporate_costs / i.total_districts))::numeric,2) as true_profit
 from int_finance i
 join risk r on i.business_unit_id = r.business_unit_id
 group by 1, 2
